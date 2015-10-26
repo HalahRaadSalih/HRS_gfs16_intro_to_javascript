8ad71fb0a126b402d2cbed9b0ba14ce43eae9128
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-10-26 14:52:24 -0700

    fixed nested values example

--- nestedValues.js
+++ nestedValues.js
@@ -1,11 +1,15 @@
 var users = [];
 
-for(var i = 0; i<5; i++){
+for(var i = 0; i <= 5; i++){
 	var user = {
-		firstName= "Halah" + i,
-		lastName = "Salih" + i,
+		firstName: "Halah" + i,
+		lastName: "Salih" + i,
 	};
 
 	user.parents= [];
-	user.parents.firstName = i + 'firstName';
+
+	user.parents.push('jane');
+	user.parents.push('smith');
+
+	users.push(user);
 }
