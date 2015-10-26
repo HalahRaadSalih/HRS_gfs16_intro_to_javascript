30cd7b04a02016d4a41d78374c9d6696ee66ac8e
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-10-26 13:30:35 -0700

    intial commit

---
+++ intro_to_js_part2.js
@@ -0,0 +1,24 @@
+var myArr = [23,335,34,67,74,244,53535,34,292479874,111,345];
+
+for(var i = 0; i < myArr.length; i++){
+    
+    console.log(myArr[i]);
+}
+
+
+var person = {  firstName: "Halah",
+                lastName:"Salig",
+                phoneNumber:1838131837387, 
+                email:"halah@gmail.com"
+};
+
+console.log(person.firstName);
+console.log(person.lastName);
+
+person.first_name="jseffehefu";
+person.last_name = "i487837";
+
+console.log(person);
+
+delete person.email;
+console.log(person);
